65250b9622ef46b3c772138775cc053a7cfdde68
Ramid Khan <ramidzkh@gmail.com>
2020-12-06 15:04:10 +1100

    Setup publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,16 +17,65 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+plugins {
+    base
+}
+
 allprojects {
     group = "io.github.f2bb.amalgamation"
     version = "1.0.0-SNAPSHOT"
+}
+
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "maven-publish")
+    apply(plugin = "signing")
+
+    extensions.getByType<JavaPluginExtension>().apply {
+        withJavadocJar()
+        withSourcesJar()
+
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 
     tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+
         if (JavaVersion.current().isJava9Compatible) {
             options.release.set(8)
         } else {
             sourceCompatibility = "8"
             targetCompatibility = "8"
+        }
+    }
+
+    tasks.withType<Javadoc> {
+        if (JavaVersion.current().isJava9Compatible) {
+            (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+        }
+    }
+
+    extensions.getByType<PublishingExtension>().apply {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                from(components["java"])
+
+                extensions.getByType<SigningExtension>().apply {
+                    if (signatory != null) {
+                        sign(this@create)
+                    }
+                }
+            }
+        }
+
+        repositories {
+            maven {
+                val releasesRepoUrl = uri("${rootProject.buildDir}/repos/releases")
+                val snapshotsRepoUrl = uri("${rootProject.buildDir}/repos/snapshots")
+                name = "Project"
+                url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            }
         }
     }
 }
