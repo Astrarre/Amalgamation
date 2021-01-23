eb1b9c2e9f3a61e40faaea1beab0bf162f5a4471
Ramid Khan <ramidzkh@gmail.com>
2021-01-23 19:19:32 +1100

    Update copyright

--- subprojects/gradle-plugin/src/main/java/io/github/f2bb/amalgamation/gradle/tasks/RemapJar.java
+++ subprojects/gradle-plugin/src/main/java/io/github/f2bb/amalgamation/gradle/tasks/RemapJar.java
@@ -1,3 +1,22 @@
+/*
+ * Amalgamation
+ * Copyright (C) 2021 Astrarre
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 package io.github.f2bb.amalgamation.gradle.tasks;
 
 import io.github.f2bb.amalgamation.gradle.impl.MappingUtils;
@@ -9,23 +28,20 @@ import org.cadixdev.lorenz.model.FieldMapping;
 import org.cadixdev.lorenz.model.MethodMapping;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.jvm.tasks.Jar;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 public class RemapJar extends Jar {
 
-    @Input
     private FileCollection classpath;
 
-    @Input
     private MappingSet mappings;
 
+    @InputFiles
     public FileCollection getClasspath() {
         return classpath;
     }
@@ -34,7 +50,12 @@ public class RemapJar extends Jar {
         this.classpath = classpath;
     }
 
-    public void mappings(MappingSet mappings) {
+    @Input
+    public MappingSet getMappings() {
+        return mappings;
+    }
+
+    public void setMappings(MappingSet mappings) {
         this.mappings = mappings;
     }
 
@@ -58,17 +79,14 @@ public class RemapJar extends Jar {
                 .build();
 
         FileCollection inputs = getInputs().getFiles();
-        List<CompletableFuture<?>> futures = new ArrayList<>();
 
         for (File file : inputs) {
-            futures.add(remapper.readInputsAsync(file.toPath()));
+            remapper.readInputsAsync(file.toPath());
         }
 
         for (File file : classpath) {
-            futures.add(remapper.readClassPathAsync(file.toPath()));
+            remapper.readClassPathAsync(file.toPath());
         }
-
-        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
 
         try (OutputConsumerPath outputConsumerPath = new OutputConsumerPath.Builder(getArchiveFile().get().getAsFile().toPath()).build()) {
             for (File file : inputs) {
