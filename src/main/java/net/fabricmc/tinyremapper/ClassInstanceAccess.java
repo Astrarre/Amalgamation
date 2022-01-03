dbfab96f911b915170440eedcfb655c468eb6005
Devan-Kerman <dev.sel20@gmail.com>
2022-01-02 22:32:05 -0600

    ready for release(?)
    Signed-off-by: Devan-Kerman <dev.sel20@gmail.com>

--- src/main/java/net/fabricmc/tinyremapper/ClassInstanceAccess.java
+++
@@ -1,9 +0,0 @@
-package net.fabricmc.tinyremapper;
-
-import net.fabricmc.tinyremapper.api.TrClass;
-
-public class ClassInstanceAccess {
-	public static InputTag[] getInputTags(TrClass type) {
-		return ((ClassInstance)type).getInputTags();
-	}
-}
