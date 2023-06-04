b15222841702472ff15ccd100c066be6c303b967
ueaj <dev.sel20@gmail.com>
2023-06-04 00:23:59 -0500

    back from the dead

---
+++ src/main/java/net/devtech/amalgamation/MinecraftAmalgamationGradlePlugin.java
@@ -0,0 +1,11 @@
+package net.devtech.amalgamation;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class MinecraftAmalgamationGradlePlugin implements Plugin<Project> {
+	@Override
+	public void apply(Project target) {
+
+	}
+}
